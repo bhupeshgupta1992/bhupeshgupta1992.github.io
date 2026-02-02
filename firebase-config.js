4c7bf32acca3a1492df3bf0d98fb0c65f8700112
bhupeshgupta1992 <bhupeshgupta1992@gmail.com>
2026-02-02 21:47:36 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnvNGbKUWh_wQT76mELb1ye5G4ew-Fmms",
+  apiKey: "AIzaSyDnNVGBKUHh_wQT7GmELb1ye5G4eW-Fmms",
   authDomain: "ijmch-bf355.firebaseapp.com",
   projectId: "ijmch-bf355",
   storageBucket: "ijmch-bf355.appspot.com",
   messagingSenderId: "218231333552",
-  appId: "1:218231333552:web:30e90d80aad616c9711175"
+  appId: "1:218231333552:web:36e96d80aad616c97f1175",
+  measurementId: "G-FRLL9JT2ER"
 };
 
 const app = initializeApp(firebaseConfig);
